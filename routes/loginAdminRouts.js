a55dff297bc43b778ea36e316407c144d8263dbe
Mariusz Podgorski <m.b.podgorski1984@gmail.com>
2021-04-01 18:37:36 +0200

    heroku deployment

--- routes/loginAdminRouts.js
+++ routes/loginAdminRouts.js
@@ -4,38 +4,25 @@ const Article = require('../models/article.model');
 
 function loginAdminRouter(app) {
 
-
-
     const password = process.env.loginPasword;
     let login = process.env.login;
-
-
-
-    
 
    app.get('/api/post', async (req, res) => {
 
          await Article.find()
             .then(ArticleList => res.json(ArticleList))
             .catch(err => res.status(400).json('Error: ' + err)) 
-
     }) 
 
 
     app.post('/login', (req, res) => {
-
         const passwordSt = req.body.permission.passwordSt
         const loginSt = req.body.permission.loginSt
-
-
         if (password === passwordSt && login === loginSt) {
-
-
             res.json({
                 permission: true
             })
         } else {
-
             res.json({
                 permission: false
             })
@@ -43,18 +30,13 @@ function loginAdminRouter(app) {
 
     })
 
-
-
     app.get('/admin/messages', (req, res) => {
-
         Message.find()
             .then(commentsList => res.json(commentsList))
             .catch(err => res.status(400).json('Error: ' + err))
-
     })
 
     app.delete('/admin/messages', (req, res) => {
-
         Message.findByIdAndDelete(req.body.id)
             .then(() => res.json('Exersise deleted. '))
             .catch(err => res.status(400).json('Error: ' + err))
@@ -63,14 +45,8 @@ function loginAdminRouter(app) {
 
 
     app.post('/admin/add-article', (req, res) => {
-
-
-
         let route = 'article/';
-
         route += req.body.title;
-
-        console.log(route);
 
         const article = new Article({
             title: req.body.title,
@@ -80,8 +56,6 @@ function loginAdminRouter(app) {
         article.save(req.body.id)
             .then(() => res.json('Article added. '))
             .catch(err => res.status(400).json('Error: ' + err))
-
-
     })
 
     app.get('/admin/confirm-comments', (req, res) => {
@@ -91,8 +65,6 @@ function loginAdminRouter(app) {
             .catch(err => res.status(400).json('Error: ' + err))
 
     })
-
-
 
     app.delete('/admin/confirm-comments', (req, res) => {
 
@@ -119,17 +91,11 @@ function loginAdminRouter(app) {
 
     app.get('/article/:title', (req, res) => {
 
-
         Comments.find()
             .then(commentsList => res.json(commentsList))
             .catch(err => res.status(400).json('Error: ' + err))
 
     })
-
-
-
-
-
 }
 
 module.exports = loginAdminRouter;
